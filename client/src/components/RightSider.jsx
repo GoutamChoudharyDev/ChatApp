8c92abd1208657a0ff157c8fe5f597f38f9a274f
unknown <rc.goutam.choudhary@gmail.com>
2025-12-06 10:00:31 +0530

    setup tailwind, google fonts, router, pages

---
+++ client/src/components/RightSider.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RightSider = () => {
+  return (
+    <div>
+      RightSider
+    </div>
+  )
+}
+
+export default RightSider
---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react"
+import ChatContainer from "../components/ChatContainer"
+import RightSider from "../components/RightSider"
+import Sidebar from "../components/Sidebar"
+
+const HomePage = () => {
+
+  // UseState
+  const [selectedUser, setSelectedUser] = useState(false)
+
+  return (
+    <div className="w-full h-screen sm:px-[15%] sm:py-[5%]">
+      <div className={`bcakdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-[100%] grid grid-cols-1 relative ${selectedUser ? "md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]" : "md:grid-cols-2"}`}>
+        <Sidebar />
+        <ChatContainer />
+        <RightSider />
+      </div>
+    </div>
+  )
+}
+
+export default HomePage
